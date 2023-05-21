f7da4528db8d6f67ffd876818e9397e803e9ab28
成龙生 <1536463948@qq.com>
2023-05-21 19:12:17 +0800

    feat: 添加CacheManager日志打印

--- src/main/java/com/loong/novel/NovelApplication.java
+++ src/main/java/com/loong/novel/NovelApplication.java
@@ -1,17 +1,38 @@
 package com.loong.novel;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
 
 @SpringBootApplication
 @MapperScan("com.loong.novel.dao.mapper")
 @EnableCaching
+@Slf4j
 public class NovelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NovelApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return args -> {
+            Map<String, CacheManager> cacheManagerMap = context.getBeansOfType(CacheManager.class);
+            log.info("loaded cache manager >>");
+            cacheManagerMap.forEach((k, v) -> {
+                log.info("{}: {}", k, v.getClass().getName());
+                log.info("caches: {}", v.getCacheNames());
+            });
+            log.info("<<");
+        };
     }
 
 }
